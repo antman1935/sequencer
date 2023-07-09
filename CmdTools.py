a832a2cbd80881b1442e0f48955cc1696f5af604
Anthony Simpson <anthony.lamont99@yahoo.com>
2023-07-09 00:11:08 -0400

    Initial commit. Just has fubini generation.

---
+++ CmdTools.py
@@ -0,0 +1,117 @@
+﻿# author: antman1935, anthony.lamont99@yahoo.com
+
+from enum import StrEnum
+
+"""
+Encapsulates the logic for validating different data types of parameters that can
+be passed to the generators.
+"""
+class ParamType(StrEnum):
+    NATURAL = "Natural number"
+    INTEGER = "Integers"
+    INT_POS = "Positive integers"
+
+    """
+    This function validates the parameter based on the ParamType and returns the valid
+    data in the correct datatype.
+    """
+    def validateAndConvertParameter(param_type: ParamType, value):
+        match param_type:
+            case ParamType.NATURAL:
+                # ℕ, value must be integer and >= 0
+                conv_value = int(value)
+                assert conv_value > -1, "Natural numbers should be greater than -1."
+                return conv_value
+            case ParamType.INTEGER:
+                # ℤ, value must be integer
+                return int(value)
+            case ParamType.INT_POS:
+                # ℤ^+, value must be positive integer
+                conv_value = int(value)
+                assert conv_value > 0, "Positive integers should be greater than 0."
+                return conv_value
+            case _:
+                raise Exception("Unsupported parameter type:", param_type)
+
+ParamType.validateAndConvertParameterValue = staticmethod(ParamType.validateAndConvertParameterValue)
+
+"""
+A command defines the list of the parameters that can be passed to it, and which ones are required.
+"""
+class CommandParameter:
+    def __init__(self, name: str, required: bool, param_type: ParamType, description: str):
+        self.name = name
+        self.required = required
+        self.param_type = param_type
+        self.description = description
+
+"""
+Given a list of parameters, allows for a parameter string matching those parameters to be parsed and
+validated.
+"""
+class CommandParser:
+    def __init__(self, parameter_list: list[CommandParameter]):
+        self.parameters = {}
+        for param in parameter_list:
+            self.parameters[param.name] = param
+
+    """
+    This function takes a string of parameters and extracts them for use in the specfied command.
+    The functions returns the options in a dictionary from param name to value.
+    The param_str is of the form
+        param1_name:param1_value[|paramN_name:oaramN_value]
+    This function also guarantees that all required parameters are set.
+    """
+    def parseInput(self, param_str: str):
+        params = param_str.split("|")
+        param_dict = {}
+        for param_pair in params:
+            [param_name, param_value] = param_pair.split(":")
+            assert param_name in self.parameters, f"Invalid parameter passed: {param_name}"
+
+            value = ParamType.validateAndConvertParameter(self.parameters[param_name].param_type, param_value)
+            param_dict[param_name] = value
+
+        for name, param in self.parameters.items():
+            if param.required:
+                assert name in param_dict, f"Required parameter {name} is not set."
+
+        return param_dict
+
+
+"""
+Base class for all of the commands we'll create. Has a generator function that should
+be overriden by all the subclasses.
+"""
+class Command:
+    def generator(self):
+        raise Exception("unimplemented")
+
+    """
+    Convienence function for testing generator output. Given a command that generates words, check that
+    its output matches the list. Gives delta from both list if there is a miss-match.
+    """
+    def generator_test(name: str, cmd: Command, expected_list):
+        print(name)
+        cmd_m_expected_list = []
+        expected_list_m_cmd = []
+
+        temp = []
+        for word in cmd.generator():
+            if not word in expected_list:
+                cmd_m_expected_list.append(word)
+            else:
+                temp.append(word)
+
+        for word in expected_list:
+            if not word in temp:
+                expected_list_m_cmd.append(word)
+
+        if len(cmd_m_expected_list) > 0 or len(expected_list_m_cmd) > 0:
+            print(f"Generator test failed.\n\tExtra entries generated: {cmd_m_expected_list}.\n\tExpected entries missed: {expected_list_m_cmd}.")
+            return False
+    
+        print("Success!")
+        return True
+
+Command.generator_test = static_method(Command.generator_test)
---
+++ FubiniRankings.py
@@ -0,0 +1,173 @@
+# author: antman1935, anthony.lamont99@yahoo.com
+
+#####################################################################
+#                                                                   #
+# Configuration Code                                                #
+#                                                                   #
+#####################################################################
+
+from CmdTools import CommandParameter, ParamType, CommandParser, Command
+
+"""
+This class takes in a string with all the necessary and optional
+parameters for this generator and gives access to the generator with
+those parameters.
+"""
+class FubiniGeneratorCmd(Command):
+    parameters: list[CommandParameter] = [
+        CommandParameter("n", True, ParamType.NATURAL, "The number of positions to give a rank."),
+        CommandParameter("k", False, ParamType.NATURAL, "The maximum number of positions in an individual tie."),
+        CommandParameter("t", False, ParamType.NATURAL, "The maximum number of ties."),
+    ]
+    parser: CommandParser = CommandParser(parameters)
+
+
+    def __init__(self, param_str: str):
+        params = FubiniGeneratorCmd.parser.parseInput(param_str)
+        self.options = FubiniGeneratorOptions(params["n"], params.get("k", None), params.get("t", None))
+
+    def generator(self):
+        for word in generateFubiniRankings(self.options):
+            yield word
+        
+
+"""
+This class allows us to pass arguments to the fubini ranking
+generator below to allow any of the following restrictions to the
+rankings we generate:
+
+1. n - The number of positions to give rankings.
+2. k - The maximum number of positions in an individual tie. E.g
+       [1,2,2,4,4,6,6,6] has 2 ties between two positions (2/4) and 1
+       tie between 3 positions, so k < 3 would cause this fubini
+       ranking to be omitted by the generator.
+3. t - The maximum number of ties. [1,2,2,4,4,6,6,6] has 3 ties, so
+       t < 3 would cause this fubini ranking to be omitted.
+"""
+class FubiniGeneratorOptions:
+    def __init__(self, n, k = None, t = None):
+        self.n = n
+        self.k = k
+        self.t = t
+
+#####################################################################
+#                                                                   #
+# Generator Code                                                    #
+#                                                                   #
+#####################################################################
+
+from typing import Optional
+
+def placeAvailable(i: int, places: dict[int, int], k: Optional[int] = None, t: Optional[int] = None):
+    check_i = places[i] + i
+    if t is not None and places[i] > 0 and sum([places[j] > 1 for j in places if j != i]) == t:
+        return False
+    if k is not None and places[i] == k:
+        return False
+    if check_i > len(places) or places[check_i] > 0:
+        return False
+    if sum([places[j] + j - 1 >= check_i for j in places if j < i]) > 0:
+        return False
+    return True
+
+def generateFubiniRankings(options: FubiniGeneratorOptions):
+    def helper(current_ranking, places):
+        if len(current_ranking) == options.n:
+            yield current_ranking[:]
+        else:
+            for i in range(1, options.n+1):
+                if placeAvailable(i, places, options.k, options.t):
+                    places[i] += 1
+                    current_ranking.append(i)
+                    for ranking in helper(current_ranking, places):
+                        yield ranking
+                    places[i] -= 1
+                    current_ranking.pop()
+        return
+
+    for ranking in helper([], {i+1:0 for i in range(options.n)}):
+        yield ranking
+
+def getTies(ranking: list[int]):
+    places = [0 for i in ranking]
+    for rank in ranking:
+        places[rank-1] += 1
+    return sum([rankcount > 1 for rankcount in places])
+
+def getRanksInTies(ranking: list[int]):
+    places = [0 for i in ranking]
+    for rank in ranking:
+        places[rank-1] += 1
+    return sum([rankcount if rankcount > 1 else 0 for rankcount in places])
+
+#####################################################################
+#                                                                   #
+# Tests                                                             #
+#                                                                   #
+#####################################################################
+
+from CmdTools import generator_test
+
+def base_generator_test():
+    param_str = "n:3"
+    expected_values = [
+        [1,1,1],
+        [1,1,3],
+        [1,3,1],
+        [3,1,1],
+        [1,2,2],
+        [2,1,2],
+        [2,2,1],
+        [1,2,3],
+        [1,3,2],
+        [2,1,3],
+        [2,3,1],
+        [3,1,2],
+        [3,2,1],
+    ]
+
+    cmd = FubiniGeneratorCmd(param_str)
+    return Command.generator_test(f"Running Fubini generator test ({param_str})", cmd, expected_values)
+
+def test_k_generator():
+    param_str = "n:3|k:2"
+    expected_values = [
+        [1,1,3],
+        [1,3,1],
+        [3,1,1],
+        [1,2,2],
+        [2,1,2],
+        [2,2,1],
+        [1,2,3],
+        [1,3,2],
+        [2,1,3],
+        [2,3,1],
+        [3,1,2],
+        [3,2,1],
+    ]
+
+    cmd = FubiniGeneratorCmd(param_str)
+    return Command.generator_test(f"Running Fubini generator test ({param_str})", cmd, expected_values)
+
+def test_t_generator():
+    param_str = "n:3|k:3|t:0"
+    expected_values = [
+        [1,2,3],
+        [1,3,2],
+        [2,1,3],
+        [2,3,1],
+        [3,1,2],
+        [3,2,1],
+    ]
+
+    cmd = FubiniGeneratorCmd(param_str)
+    return Command.generator_test(f"Running Fubini generator test ({param_str})", cmd, expected_values)
+
+def tests():
+    test_funcs = [base_generator_test, test_k_generator, test_t_generator]
+    print(f"Running {len(test_funcs)} tests for Fubini Generator.")
+    success = sum([func() for func in test_funcs])
+    print(f"{success}/{len(test_funcs)} tests passed.")
+
+if __name__ == "__main__":
+    tests()
